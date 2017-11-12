b36387239642f33075b7925b566a426c504e5e11
paswd <paswd@yandex.ru>
2017-11-12 23:27:41 +0300

    Added building map

--- musiclib.cpp
+++ musiclib.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <cassert>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <cmath>
 #include <vector>
@@ -11,4 +12,50 @@
 
 using namespace std;
 
+void TMusicLib::Build(char *filename) {
+	//Get filenames from file
+	FILE *in = fopen(filename, "r");
 
+	if (in == NULL) {
+		cout << "ERROR: Couldn't open file" << endl;
+		return;
+	}
+	char tmp;
+	string tmp_str = "";
+
+	while (fscanf(in, "%c", &tmp) != EOF) {
+		if (tmp == '\n') {
+			if (!tmp_str.empty) {
+				this->Files.push_back(tmp_str);
+			}
+			tmp_str = "";
+			continue;
+		}
+		tmp_str += tmp;
+	}
+	if (!tmp_str.empty) {
+		this->Files.push_back(tmp_str);
+	}
+
+	//Building map
+	vector <size_t> hash_arr;
+
+	for (size_t i = 0; i < this->Files.size(); i++) {
+		ReadSingleMP3(this->Files[i].c_str(), &hash_arr, &this->Mh);
+		for (size_t j = 0; j < hash_arr.size(); j++) {
+			this->Lib[hash_arr[j]].push_back(make_pair(i, j));
+		}
+	}
+}
+
+void TMusicLib::Export(char *filename) {
+	
+}
+
+void TMusicLib::Import(char *filename) {
+	
+}
+
+string TMusicLib::Check(char *filename) {
+	
+}
--- musiclib.h
+++ musiclib.h
@@ -8,10 +8,21 @@
 #include <cstring>
 #include <cmath>
 #include <vector>
+#include <string>
 #include <mpg123.h>
+#include "tmpg123.h"
 
-class TMusicMap {
+class TMusicLib {
+private:
+	std::vector <std::string> Files(0);
+	std::map <size_t, std::vector <std::pair<size_t, size_t>>> Lib(0);
+	TMpg123 Mh;
 
+public:
+	void Build(char *filename);
+	void Export(char *filename);
+	void Import(char *filename);
+	std::string Check(char *filename);
 };
 
 #endif //MUSICLIB_H
